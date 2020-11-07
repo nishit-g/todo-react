93430534d7a32ba4a7204de0b0016c2804a46385
nishit-g <nishit.gupta6708@gmail.com>
2020-11-07 18:30:56 +0530

    basic add functionality

--- src/components/todoMain.js
+++ src/components/todoMain.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Todo from "./todo";
 import { getTodos } from "../services/fakeTodoService";
+import AddTodo from "./addTodo";
 
 class TodoMain extends Component {
   state = {
@@ -19,9 +20,21 @@ class TodoMain extends Component {
     this.setState({ listOfTodos });
   };
 
+  handleAddition = (todoTitle) => {
+    alert("Added TODO -> " + todoTitle);
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000),
+      title: todoTitle,
+    };
+    const listOfTodos = [newTodo, ...this.state.listOfTodos];
+    this.setState({ listOfTodos });
+  };
+
   render() {
     return (
       <div>
+        <h1>Todo Application</h1>
+        <AddTodo addNewTodo={this.handleAddition}></AddTodo>
         {this.state.listOfTodos.map((todo) => (
           <Todo key={todo.id} todo={todo} onDelete={this.handleDelete} />
         ))}
